31dce36a54e01c5c81d3bd41ccc310466169939a
Aditya Mishra <aditya.1165.mishra@gmail.com>
2025-11-12 23:54:19 +0530

    updated server.js -added route
    Added a new route to count and return the number of visits.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // server.js
+// server.js
 const express = require('express');
 const app = express();
 const PORT = 3000;
@@ -6,10 +7,20 @@ const PORT = 3000;
 app.use(express.static('public'));
 app.use(express.json());
 
+let visitCount = 0;
+
+// Existing example route
 app.get('/api/message', (req, res) => {
   res.json({ message: 'Hello from the backend!' });
+});
+
+// 👇 New visitor counter route
+app.get('/api/visits', (req, res) => {
+  visitCount++;
+  res.json({ visits: visitCount });
 });
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
+
